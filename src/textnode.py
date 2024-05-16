d67db6f93d175a305744fa5c0012a2a5bab5e657
Ian Williamson <ianwill2014@gmail.com>
2024-05-16 06:03:26 -0500

    ch2-6 still

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -9,3 +11,21 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+    def text_node_to_html_node(self):
+    
+        if self.text_type == "text_type_text":
+            return LeafNode(self.text)
+        elif self.text_type == "text_type_bold":
+            return LeafNode("b", self.text)
+        elif self.text_type == "text_type_italic":
+            return LeafNode("i", self.text)
+        elif self.text_type == "text_type_code":
+            return LeafNode("code", self.text)
+        elif self.text_type == "text_type_link":
+            return LeafNode("a", self.text, {"href":self.url})
+        elif self.text_type == "text_type_image":
+            return LeafNode("img", "", {"href":self.url, "alt":self.text})
+        else:
+            raise Exception("Text type not supported")
+        
