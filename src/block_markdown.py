73c45577bbe9d95abe730a25deb6fa8e017b5fe6
Ian Williamson <ianwill2014@gmail.com>
2024-12-20 11:20:23 -0600

    fucking up markdown_to_html_node

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,13 @@
+from htmlnode import ParentNode, LeafNode, HTMLNode
+from textnode import TextNode
+from inline_markdown import text_to_textnodes
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -13,22 +23,74 @@ def block_to_block_type(block):
     lines = block.split("\n")
 
     if block.startswith(('#', '##', '###', '####', '#####', '######')):
-        return "heading"
+        return block_type_heading
     if len(lines) > 1 and lines[0] == "```" and lines[-1] == "```":
-        return "code"
+        return block_type_code
     if block.startswith(">"):
         for line in lines:
             if not line.startswith(">"):
-                return "paragraph"
-        return "quote"
-    if block.startswith("* "):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* ") or block.startswith("- "):
         for line in lines:
             if not line.startswith("* "):
-                return "paragraph"
-        return "unordered list"
+                return block_type_paragraph
+        return block_type_ulist
     if block.startswith("1. "):
         for i in range(len(lines)):
             if not lines[i].startswith(f"{i + 1}. "):
-                return "paragraph"
-        return "ordered list"
-    return "paragraph"
+                return block_type_paragraph
+        return block_type_olist
+    return block_type_paragraph
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_heading:
+            if block_type.startswith("#"):
+                nodes.append(ParentNode("h1", text_to_textnodes(block)))
+            if block_type.startswith("##"):
+                nodes.append(ParentNode("h2", text_to_textnodes(block)))
+            if block_type.startswith("###"):
+                nodes.append(ParentNode("h3", text_to_textnodes(block)))
+            if block_type.startswith("####"):
+                nodes.append(ParentNode("h4", text_to_textnodes(block)))
+            if block_type.startswith("#####"):
+                nodes.append(ParentNode("h5", text_to_textnodes(block)))
+            if block_type.startswith("#######"):
+                nodes.append(ParentNode("h6", text_to_textnodes(block)))
+        if block_type == block_type_paragraph:
+            nodes.append(ParentNode("p", text_to_textnodes(block)))
+        if block_type == block_type_code:
+            block = block.lstrip('```\n')
+            block = block.rstrip('\n```')
+            blocknode = ParentNode("code", block)
+            nodes.append(ParentNode("pre", blocknode))
+        if block_type == block_type_quote:
+            text = ''
+            lines = block.split("\n")
+            for line in lines:
+                line = line.lstrip("> ")
+                text += line + "\n"
+            nodes.append(ParentNode("blockquote", text_to_textnodes(text.strip())))
+        if block_type == block_type_ulist:
+            child_nodes = []
+            lines = block.split("\n")
+            for line in lines:
+                print(line)
+                line = line.lstrip('- ')
+                line = line.lstrip('* ')
+                child_nodes.append(ParentNode("ul", text_to_textnodes(line)))
+            nodes.append(ParentNode("li", child_nodes))
+        if block_type == block_type_olist:
+            child_nodes = []
+            lines = block.split("\n")
+            for i in range(len(lines)):
+                line = lines[i].lstrip(f"{i + 1} ")
+                child_nodes.append(ParentNode("ol", text_to_textnodes(line)))
+            nodes.append(ParentNode("li", child_nodes))
+
+    return nodes
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -29,12 +29,46 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         block4 = block_to_block_type('* This is an \n* unordered list')
         block5 = block_to_block_type('1. This is an \n2. ordered list')
         block6 = block_to_block_type('This is a normal paragraph')
-        self.assertEqual(block1, "heading")
-        self.assertEqual(block2, "code")
-        self.assertEqual(block3, "quote")
-        self.assertEqual(block4, "unordered list")
-        self.assertEqual(block5, "ordered list")
-        self.assertEqual(block6, "paragraph")
+        self.assertEqual(block1, block_type_heading)
+        self.assertEqual(block2, block_type_code)
+        self.assertEqual(block3, block_type_quote)
+        self.assertEqual(block4, block_type_ulist)
+        self.assertEqual(block5, block_type_olist)
+        self.assertEqual(block6, block_type_paragraph)
+
+    def test_markdown_to_html_node(self):
+        markdown = """
+#### This is an h4 heading
+
+This is a 
+paragraph
+of lots of lines
+
+- This is a list
+- Of things
+* We're splitting
+* it up
+
+1. This is
+2. an ordered
+3. List
+
+> This is a
+> block quote
+> with multiple
+> lines
+
+```
+This is a
+code block
+with multiple
+lines
+```
+"""
+        result = markdown_to_html_node(markdown)
+        print(result)
+        expected = "?"
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
